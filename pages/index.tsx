e19b9b7af8c9347f72098c9f57fc9e6c7cde612d
tajultonim <mail@tajultonim.com>
2022-11-12 22:37:45 +0600

    adv

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,23 @@ const Home: NextPage<Props> = ({ posts, tags }) => {
           name="facebook-domain-verification"
           content="17feoonrfhqenwtfaw98urwul9b813"
         />
-        <link rel="canonical" href={"https://"+process.env.SITE_URL} />
+        <link rel="canonical" href={"https://" + process.env.SITE_URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={"https://" + process.env.SITE_URL} />
+        <meta property="og:image" content="https://tonim.ml/favicon.ico" />
+        <meta property="og:site_name" content="TajulTonim Blog" />
+        <meta name="twitter:site" content="@tajultonim" />
+        <meta name="twitter:title" content="TajulTonim Blog" />
+        <meta
+          name="twitter:description"
+          content="Hey, it's Tonim. It's my personal blog. I share educational stuff here."
+        />
+        <meta name="twitter:image:src" content="https://tonim.ml/favicon.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="description"
+          content="Hey, it's Tonim. It's my personal blog. I share educational stuff here."
+        />
       </Head>
       <style>{styletxt}</style>
       <Layout
