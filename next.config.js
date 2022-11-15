b3bd0d3844859a72789365c5e98ac7416dfa8923
tajultonim <mail@tajultonim.com>
2022-11-15 15:23:27 +0600

    schema change

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
   images: { domains: ["res.cloudinary.com"] },
 };
 
