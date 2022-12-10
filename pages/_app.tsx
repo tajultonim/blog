9f2e580f4f8c0f476ada972cba7d972b9e36fb3b
tajultonim <mail@tajultonim.com>
2022-12-10 20:23:18 +0600

    Partytown added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 import Head from "next/head";
+import { Partytown } from "@builder.io/partytown/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,14 +12,19 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:site_name" content="TajulTonim Blog" />
         <meta property="fb:app_id" content="531198521796306" />
+        <Partytown debug={true} forward={["dataLayer.push"]} />
       </Head>
       <Component {...pageProps} />
-      {/* Global site tag (gtag.js) - Google Analytics  */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
         strategy="afterInteractive"
+        type="text/partytown"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        type="text/partytown"
+      >
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
