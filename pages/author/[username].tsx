701d49b270e4ed0f54cc1b8cfd56eefc3b0d9afc
H.M.Tajul Islam <60362553+tajultonim@users.noreply.github.com>
2023-07-12 19:06:53 +0600

    Update [username].tsx

--- pages/author/[username].tsx
+++ pages/author/[username].tsx
@@ -49,8 +49,9 @@ const Page: NextPage<{ data: any }> = ({ data }) => {
                 />
               </div>
               <h1 className="text-2xl my-3 mt-5 font-black">{data.name}</h1>
+              <div className=" flex w-full justify-center align-center">
               <div className=" w-full mt-2">{data.bio}</div>
-              <div className=" w-full my-4 flex gap-3 mb-8">
+              <div className=" w-full my-2 flex gap-3">
                 {data.location && (
                   <PCard str={data.location} Icon={HiLocationMarker} />
                 )}
@@ -78,7 +79,7 @@ const Page: NextPage<{ data: any }> = ({ data }) => {
                     </div>
                   </Link>
                 )}
-                </div><div className=" w-full my-4 flex gap-3 mb-8">
+                </div><div className=" w-full flex gap-3 mb-8">
                 <div className=" flex gap-1">
                   {data.facebook && (
                     <SocialCard
@@ -111,6 +112,7 @@ const Page: NextPage<{ data: any }> = ({ data }) => {
                     />
                   )}
                 </div>
+                  </div>
               </div>
             </div>
             <div className=" border-t px-36 pt-6 flex justify-around  gap-2">
